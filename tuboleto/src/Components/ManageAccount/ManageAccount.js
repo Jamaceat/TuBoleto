a994b33a7e995c8b6c9b9780044f1b4f01cbbbb3
Johan <johanoriginalplus@gmail.com>
2022-12-01 23:24:22 -0500

    Adding usermanagement

--- tuboleto/src/Components/ManageAccount/ManageAccount.js
+++ tuboleto/src/Components/ManageAccount/ManageAccount.js
@@ -1,3 +1,4 @@
+import {Paper, Typography} from "@mui/material"
 import {Box} from "@mui/system"
 import React, {useContext} from "react"
 import {AllData} from "../Context/ContextProvider"
@@ -5,5 +6,31 @@ import {AllData} from "../Context/ContextProvider"
 export default function ManageAccount() {
 	const {userList} = useContext(AllData)
 
-	return <Box></Box>
+	return (
+		<Box
+			sx={{
+				display: "grid",
+				gridTemplateAreas: `" b b b b" " b b b b" " b b b b" "crud crud crud crud" "crud crud crud crud"`,
+				minHeight: "82vh",
+			}}
+		>
+			<Paper
+				sx={{
+					gridArea: "b",
+					backgroundColor: "#C3DEDB",
+					margin: "1rem",
+					borderRadius: "1rem",
+				}}
+			>
+				<Typography variant="h2" sx={{textAlign: "center"}}>
+					Información
+				</Typography>
+			</Paper>
+			{userList.sesion.type === "Seller" && (
+				<Paper sx={{gridArea: "crud", border: "1px red solid"}}>
+					{/* <Crud/> */}
+				</Paper>
+			)}
+		</Box>
+	)
 }
